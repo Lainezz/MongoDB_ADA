8d613a06e3dc0dfdf8db73ee8320818d5c3a30f9
Lainezz <diegolinaresmojacar@gmail.com>
2025-01-23 20:51:14 +0100

    clientes

--- src/main/kotlin/com/es/relacionesmongo/usuariodireccion/Cliente.kt
+++ src/main/kotlin/com/es/relacionesmongo/usuariodireccion/Cliente.kt
@@ -1,9 +1,13 @@
 package com.es.relacionesmongo.usuariodireccion
 
+import org.bson.codecs.pojo.annotations.BsonProperty
+
 data class Cliente(
-    val _id: String?,
+    val _id: String?, // EMAIL
     val nombre: String,
-    val email: String,
+    val nick: String,
+    val estado: Boolean,
+    @BsonProperty("telefonos")
     val tlfn: List<String>,
     val direccion: Direccion?
 )
--- src/main/kotlin/com/es/relacionesmongo/usuariodireccion/MainRelaciones1.kt
+++ src/main/kotlin/com/es/relacionesmongo/usuariodireccion/MainRelaciones1.kt
@@ -11,11 +11,34 @@ fun main() {
     // Obtener la colección
     val collection: MongoCollection<Cliente> = database.getCollection("collUsuarios", Cliente::class.java)
 
-    // Declarar un cliente y una direccion
-    val direccion = Direccion("Perdida", "10", "04638", "Mojacar")
-    val cliente = Cliente(null, "Pedro", "pedro@gmail.com", listOf("950475192", "677232467"), direccion)
+    try {
+        // Declarar un cliente y una direccion
+        val direccion = Direccion("alamo", "24", "04638", "Mojacar")
+        val cliente = Cliente("maria@gmail.com", "Maria", "mar14", true, listOf("950475656", "666888999"), direccion)
 
-    collection.insertOne(cliente)
+        collection.insertOne(cliente)
+
+        val direccion2 = Direccion("desconocida", "24", "04003", "Almeria")
+        val direccion3 = Direccion("principal", "2", "04003", "Almeria")
+        val direccion4 = Direccion("principal", "1", "04003", "Almeria")
+
+        val cliente2 = Cliente("pedro@gmail.com", "Pedro", "periko", true, listOf("950475656", "666888999"), direccion2)
+        val cliente3 = Cliente("ana@gmail.com", "Ana", "anuski", true, listOf("950475656", "666888999"), direccion3)
+        val cliente4 = Cliente("antonio@gmail.com", "Antonio", "toni", true, listOf("950475656", "666888999"), direccion4)
+        val cliente5 = Cliente("agustin@gmail.com", "Agustin", "agus", true, listOf("950475656", "666888999"), direccion4)
+
+        val listaClientes = listOf<Cliente>(
+            cliente2, cliente3, cliente4, cliente5
+        )
+
+        collection.insertMany(listaClientes)
+    } catch (e: Exception) {
+        println("Clave duplicada")
+    }
+
+
+
+
 
     ConexionMongo.close()
 }
